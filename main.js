71731d2d2d070b89d9098e5408f329463c5a3242
rwood219 <58787084+rwood219@users.noreply.github.com>
2023-10-10 13:01:56 -0400

    general changes

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 //Images for main image slider
 let mainImageSlider = [
-    "/Public/multiple-rings-diff-colors-unsplash.jpg",
-    "/Public/jewelry-banner.webp", 
-    "/Public/ring-black-bg.jpg",
-  ];
-  
-  let activeImage = document.getElementById("image-slider-active");
-  let currentIndex = 0; // Variable to keep track of the current image index
-  
-  function changeImage() {
-    console.log(activeImage.src);
-    activeImage.src = mainImageSlider[currentIndex];
-    currentIndex = (currentIndex + 1) % mainImageSlider.length; // Move to the next image
-  }
-  
-  // Call changeImage initially
-  changeImage();
-  
-  // Set up an interval to change images every 5 seconds
-  setInterval(changeImage, 5000);
-  
+  "/Public/multiple-rings-diff-colors-unsplash.jpg",
+ // "/Public/jewelry-banner.webp",
+  "/Public/ring-black-bg.jpg",
+];
 
-//changeImage();
+let activeImage = document.getElementById("image-slider-active");
+let currentIndex = 0;
+function changeImage(arr) {
+  activeImage.src = arr[currentIndex];
+  currentIndex = (currentIndex + 1) % arr.length;
+}
+
+changeImage(mainImageSlider);
+setInterval(() => changeImage(mainImageSlider), 5000);
