f1bfac5a3cac40be8d9647de5a8a60f1e72f695b
rwood219 <58787084+rwood219@users.noreply.github.com>
2023-10-10 10:30:37 -0400

    added image slider

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-console.log("conn");
-
-let imageSlider = ["./Public/jewerly banner.webp", "./logo.webp"];
-let testArray = [1, 2, 3, 4, 5];
-let activeImage = document.querySelector("#image-slider-active");
-
-changeImage = () => {
-  //change image and create dom emelent for each array item in imageslider
-  for (i = 0; i <= testArray.length; i++) {
-    setInterval(() => {
-      // console.log(testArray[i]);
-
-    },5000)
+//Images for main image slider
+let mainImageSlider = [
+    "/Public/multiple-rings-diff-colors-unsplash.jpg",
+    "/Public/jewelry-banner.webp", 
+    "/Public/ring-black-bg.jpg",
+  ];
+  
+  let activeImage = document.getElementById("image-slider-active");
+  let currentIndex = 0; // Variable to keep track of the current image index
+  
+  function changeImage() {
+    console.log(activeImage.src);
+    activeImage.src = mainImageSlider[currentIndex];
+    currentIndex = (currentIndex + 1) % mainImageSlider.length; // Move to the next image
   }
-};
-changeImage()
+  
+  // Call changeImage initially
+  changeImage();
+  
+  // Set up an interval to change images every 5 seconds
+  setInterval(changeImage, 5000);
+  
+
+//changeImage();
